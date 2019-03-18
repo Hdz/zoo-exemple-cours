7331892f1ac3bc7015c155cd33a977a3ec272438
DROUAUD <a.drouaud@gmail.com>
2019-03-19 00:37:05 +0100

    Premier commit
    Premier commit avec constructeurs

---
+++ src/zoo/Animal.java
@@ -0,0 +1,30 @@
+package zoo;
+
+
+public class Animal {
+	public static void main(String[] args) {
+		Animal animal = new Animal();
+		
+	}
+class Animaltest {
+	private String nom;
+	private String type;
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+
+}
+
+}
+
+
